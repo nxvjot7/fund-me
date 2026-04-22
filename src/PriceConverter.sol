6593bd960087bca82ee0418f8753b1fdb2de6c1a
Gajni <142811767+cutiapreta@users.noreply.github.com>
2026-04-22 12:11:57 +0530

    Update PriceConverter.sol

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -5,16 +5,17 @@ pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-// Why is this a library and not abstract?
-// Why not an interface?
+error PriceConverter__StalePrice();
+error PriceConverter__InvalidPrice();
+
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
     function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
-        // Sepolia ETH / USD Address
-        // https://docs.chain.link/data-feeds/price-feeds/addresses
-        (, int256 answer,,,) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+        (, int256 answer,, uint256 updatedAt,) = priceFeed.latestRoundData();
+
+        if (answer <= 0) revert PriceConverter__InvalidPrice();
+        if (block.timestamp - updatedAt > 3 hours) revert PriceConverter__StalePrice();
+
+        return uint256(answer * 1e10);
     }
 
     // 1000000000
